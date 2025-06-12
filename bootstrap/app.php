1303260d7bb4aaf7e3b64fe1a8dd73d4d5fc083a
JayTee <jaytee117@gmail.com>
2025-06-12 12:13:49 +0100

    added trustedHosts

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->use(\Illuminate\Http\Middleware\TrustHosts::class);
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {

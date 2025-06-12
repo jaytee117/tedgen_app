bd29524cf0aeba11f17f31a1ac6be0f838abfb21
JayTee <jaytee117@gmail.com>
2025-06-12 12:33:29 +0100

    adding roles

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DashboardController extends Controller
 {
     public function index(){
+        Permission::create(['name' => 'edit customers']);
         return view('dashboard');
     }
 }

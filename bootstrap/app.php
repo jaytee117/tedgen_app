cbe89a2ede53c3199cd3e9e958d607e3962e876e
JayTee <jaytee117@gmail.com>
2025-06-12 12:16:27 +0100

    removed last edit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->use(\Illuminate\Http\Middleware\TrustHosts::class);
+        
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,

1cf18306e4e9d6414181295d77715f3d48b4d2e9
jaytee117 <jaytee117@gmail.com>
2025-06-20 11:44:52 +0100

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,7 +36,7 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        return view('layouts.site.create',["user" => $user]);
+        return view('layouts.site.create');
     }
 
     /**

7caf5435aec0769486dca50ca54e51fa264f751e
JayTee <jaytee117@gmail.com>
2025-06-25 10:31:37 +0100

    Added breadcrumb functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.min.css" />
-    
+
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
@@ -27,25 +27,14 @@
             </div>
         @endif
         <header>
-            <nav class="relative flex items-center w-full px-4">
-                <!-- Mobile Header -->
-                <div class="inline-flex items-center justify-center w-full md:hidden">
-                    <a href="#" @click="open = true" @click.away="open = false" class="absolute left-0 pl-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 stroke-blue-600" fill="currentColor"
-                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
-                    </a>
-                </div>
-                @auth
-                    <span class="border-r-2 pr-2">Hi there, {{ Auth::user()->name }}</span>
-                    @if (request()->routeIs('customer.index'))
-                        <a href="{{ route('customer.create') }}">Create New Customer</a>
-                    @endif
-                    @include('layouts.sidebar')
-                @endauth
+            @auth
+                @if (request()->routeIs('customer.index'))
+                    <a href="{{ route('customer.create') }}" class="btn float-end">Create New Customer</a>
+                @endif
 
-            </nav>
+                @include('layouts.sidebar')
+            @endauth
+            {{ Breadcrumbs::render() }}
         </header>
 
         <main class=" min-h-[200px] pt-4 w-full px-4 mx-auto">
---
+++ routes/breadcrumbs.php
@@ -0,0 +1,44 @@
+<?php
+use App\Models\Customer;
+use App\Models\Site;
+use App\Models\Installation;
+use App\Models\User;
+use Diglactic\Breadcrumbs\Breadcrumbs;
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+ 
+Breadcrumbs::for('dashboard', function (BreadcrumbTrail $trail): void {
+    $trail->push('Dashboard', route('dashboard'));
+});
+
+Breadcrumbs::for('customer.index', function (BreadcrumbTrail $trail): void {
+    $trail->push('Customers', route('customer.index'));
+});
+
+Breadcrumbs::for('site.index', function (BreadcrumbTrail $trail): void {
+    $trail->push('Sites', route('site.index'));
+});
+
+Breadcrumbs::for('installation.index', function (BreadcrumbTrail $trail): void {
+    $trail->push('Installations', route('installation.index'));
+});
+
+Breadcrumbs::for('customer.show', function (BreadcrumbTrail $trail, Customer $customer): void {
+    $trail->parent('customer.index');
+    $trail->push($customer->customer_name, route('customer.show', $customer));
+});
+
+Breadcrumbs::for('site.show', function (BreadcrumbTrail $trail, Site $site): void {
+    $customer = Customer::find($site->account_id);
+    $trail->parent('customer.show', $customer);
+    $trail->push($site->site_name, route('site.show', $site));
+});
+
+Breadcrumbs::for('installation.show', function (BreadcrumbTrail $trail, Installation $installation): void {
+    $site = Site::find($installation->site_id);
+    $trail->parent('site.show', $site);
+    $trail->push($installation->asset_id, route('installation.show', $installation));
+});
+
+Breadcrumbs::for('user.show', function (BreadcrumbTrail $trail): void {
+    $trail->push('User Profile', route('user.show'));
+});

8358d0b22c1f1abcf34b5b0c7a205ec32c3ce5e4
JayTee <jaytee117@gmail.com>
2025-06-26 15:27:52 +0100

    started to build scheduler

---
+++ app/Console/Commands/Get2GReportsCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class Get2GReportsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:get2-g-reports-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Log::info('Get 2G Reports scheduling');
+    }
+}

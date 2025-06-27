595c2c8c55e6d2547c537ec7d869f1a5a497a160
JayTee <jaytee117@gmail.com>
2025-06-27 10:59:08 +0100

    friday commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,43 @@
-<x-app-layout>
-    <form action="{{ route('login') }}" method="POST">
-        @csrf
-        <h2>Log In to Your Account</h2>
-        <label for="email" >Email</label>
-        <input type="email" name="email" required value="{{ old('email') }}">
-        <label for="password" >Password</label>
-        <input type="password" name="password" required>
-        <button type="submit" class="btn mt-4">Log In</button>
-        <!-- validation errors -->
-        @if($errors->any())
-            <ul class="px-4 py-2 bg-red-100">
-                @foreach($errors->all() as $error)
-                    <li class="my-2 text-red-500">{{ $error }}</li>
-                @endforeach
-            </ul>
-        @endif
-    </form>
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TED Generation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    <main class="container">
+        <form action="{{ route('login') }}" method="POST" style="margin-top:100px">
+            @csrf
+            <h2>Log In to Your Account</h2>
+            <label for="email">Email</label>
+            <input type="email" name="email" required value="{{ old('email') }}" class="form-control">
+            <label for="password">Password</label>
+            <input type="password" name="password" required class="form-control">
+            <button type="submit" class="btn btn-success mt-4">Log In</button>
+            <!-- validation errors -->
+            @if ($errors->any())
+                <ul class="px-4 py-2 bg-red-100">
+                    @foreach ($errors->all() as $error)
+                        <li class="my-2 text-red-500">{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
+        </form>
+    </main>
+</body>
+
+</html>
+
+
 
--- resources/views/theme/sidebar.blade.php
+++ resources/views/theme/sidebar.blade.php
@@ -3,7 +3,7 @@
         <div class="sb-sidenav-menu">
             <div class="nav">
                 <div class="sb-sidenav-menu-heading">Core</div>
-                <a class="nav-link " href="/dashboard" >
+                <a class="nav-link " href="/dashboard">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard
                 </a>
@@ -28,7 +28,9 @@
         </div>
         <div class="sb-sidenav-footer">
             <div class="small">Logged in user:</div>
-            {{ auth()->user()->name }}
+            @if (isset(auth()->user()->name))
+                {{ auth()->user()->name }}
+            @endif
         </div>
     </nav>
 </div>

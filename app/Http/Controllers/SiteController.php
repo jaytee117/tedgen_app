f3a4126d2b80a67028a8ae139a1fa7bbf0220b57
JayTee <jaytee117@gmail.com>
2025-07-01 10:36:39 +0100

    test

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -12,6 +12,7 @@ class SiteController extends Controller
 {
     public function index()
     {
+        dd('test');
         $sites = Site::with('account')->orderBy('created_at', 'desc')->paginate(10);
         return view('layouts.site.index', ['sites' => $sites]);
     }

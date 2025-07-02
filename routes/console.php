c60b7df73b96f0513c930262f898ce6736c14f13
jaytee117 <jaytee117@gmail.com>
2025-07-02 17:07:00 +0100

    fix

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:get2-g-reports-command')->hourlyAt(5);
+Schedule::command('app:get2-g-reports-command')->hourlyAt(8);
 
 Schedule::command('app:fetch-weather-command')->hourlyAt(14);

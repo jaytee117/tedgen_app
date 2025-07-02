f2edbaf9cfcbd5f6f14a70b33b8e9b84ab2463d5
jaytee117 <jaytee117@gmail.com>
2025-07-02 19:34:04 +0100

    changed run time 2g api

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:get2-g-reports-command')->hourlyAt(30);
+Schedule::command('app:get2-g-reports-command')->hourlyAt(35);
 
 Schedule::command('app:fetch-weather-command')->hourlyAt(14);

bb2080c81e363c90c9df04db75c28a9553cab392
jaytee117 <jaytee117@gmail.com>
2025-07-03 10:43:17 +0100

    dash img

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -47,10 +47,10 @@
             <div class="card mb-4">
                 <div class="card-header">
                     <i class="fas fa-chart-area me-1"></i>
-                    {{ $install->id }}
+                    {{ $install->site['site_name'] }}
                 </div>
                 <div class="card-body">
-                    <img src="storage/{{ $install->site['site_img'] }}" alt="Uploaded File" id="preview">
+                    <img src="storage/{{ $install->site['site_img'] }}" alt="Uploaded File" id="preview" height="200">
                 </div>
             </div>
         </div>

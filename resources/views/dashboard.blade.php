f347a564b314cd5f0ef131f92593b4e955fb4bf8
jaytee117 <jaytee117@gmail.com>
2025-07-03 10:45:57 +0100

    dash img

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -50,7 +50,7 @@
                     {{ $install->site['site_name'] }}
                 </div>
                 <div class="card-body">
-                    <img src="storage/{{ $install->site['site_img'] }}" alt="Uploaded File" id="preview" style="height:180px">
+                    <img src="storage/{{ $install->site['site_img'] }}" alt="Uploaded File" id="preview" >
                 </div>
             </div>
         </div>

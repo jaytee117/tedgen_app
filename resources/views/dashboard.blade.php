c3e115f6158c6b18809271a8b543591e9f3c9da9
jaytee117 <jaytee117@gmail.com>
2025-07-03 10:28:38 +0100

    dash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -42,17 +42,17 @@
         </div>
     </div>
     <div class="row">
-    @foreach($installations as $install)
+        @foreach($installations as $install)
         <div class="col-xl-6">
             <div class="card mb-4">
                 <div class="card-header">
                     <i class="fas fa-chart-area me-1"></i>
                     {{ $install->id }}
                 </div>
-                <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
+                <div class="card-body"></div>
             </div>
         </div>
-    @endforeach
+        @endforeach
     </div>
     <div class="row">
         <div class="col-xl-6">

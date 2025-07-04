a5cbae8dd560ffce63182bb00caf4c9b8c6f1492
jaytee117 <jaytee117@gmail.com>
2025-07-04 13:06:15 +0100

    more work on dashboard layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,16 +2,12 @@
 
 @section('content')
 <div class="container-fluid px-4">
-    <h3 class="mt-4">Dashboard</h3>
     <div class="row">
         <div class="col-xl-3 col-md-6">
             <div class="card bg-primary text-white mb-4">
-                <div class="row">
-                    <div class="col-md-4">
-                        <img src="" style="width: 100px;margin: 0 auto;">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="h2" style="font-size:24px;font-weight:100;margin-top:12px">
+                <div class="row">                    
+                    <div class="col-md-12">
+                        <div class="h2" style="font-size:24px;font-weight:100;">
                             Today
                         </div>
                     </div>
@@ -29,46 +25,83 @@
                         <div class="small text-white stretched-link">Heat Generated</div>
                         <div class="small text-white"><i class="fas fa-angle-right"></i> 99 Kw</div>
                     </div>
-
-
-
-                </div>
-                
-            </div>
-            <!--<div class="card bg-primary text-white mb-4">
-                <div class="card-body">Primary Card</div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
-                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div>-->
+                </div>                
+            </div>            
         </div>
         <div class="col-xl-3 col-md-6">
-            <div class="card bg-warning text-white mb-4">
-                <div class="card-body">Warning Card</div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
-                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+            <div class="card bg-primary text-white mb-4">
+                <div class="row">                    
+                    <div class="col-md-12">
+                        <div class="h2" style="font-size:24px;font-weight:100;">
+                            Yesterday
+                        </div>
+                    </div>
                 </div>
-            </div>
+                <div class="row">
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <div class="small text-white stretched-link">Gas Consumed</div>
+                        <div class="small text-white"><i class="fas fa-angle-right"></i> 99 Kw</div>
+                    </div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <div class="small text-white stretched-link">Electricity Generated</div>
+                        <div class="small text-white"><i class="fas fa-angle-right"></i> 99 Kw</div>
+                    </div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <div class="small text-white stretched-link">Heat Generated</div>
+                        <div class="small text-white"><i class="fas fa-angle-right"></i> 99 Kw</div>
+                    </div>
+                </div>                
+            </div> 
         </div>
         <div class="col-xl-3 col-md-6">
-            <div class="card bg-success text-white mb-4">
-                <div class="card-body">Success Card</div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
-                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+            <div class="card bg-primary text-white mb-4">
+                <div class="row">                    
+                    <div class="col-md-12">
+                        <div class="h2" style="font-size:24px;font-weight:100;">
+                            Month To Date
+                        </div>
+                    </div>
                 </div>
-            </div>
+                <div class="row">
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <div class="small text-white stretched-link">Gas Consumed</div>
+                        <div class="small text-white"><i class="fas fa-angle-right"></i> 99 Kw</div>
+                    </div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <div class="small text-white stretched-link">Electricity Generated</div>
+                        <div class="small text-white"><i class="fas fa-angle-right"></i> 99 Kw</div>
+                    </div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <div class="small text-white stretched-link">Heat Generated</div>
+                        <div class="small text-white"><i class="fas fa-angle-right"></i> 99 Kw</div>
+                    </div>
+                </div>                
+            </div> 
         </div>
         <div class="col-xl-3 col-md-6">
-            <div class="card bg-danger text-white mb-4">
-                <div class="card-body">Danger Card</div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
-                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+            <div class="card bg-primary text-white mb-4">
+                <div class="row">                    
+                    <div class="col-md-12">
+                        <div class="h2" style="font-size:24px;font-weight:100;">
+                            Year To Date
+                        </div>
+                    </div>
                 </div>
-            </div>
+                <div class="row">
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <div class="small text-white stretched-link">Gas Consumed</div>
+                        <div class="small text-white"><i class="fas fa-angle-right"></i> 99 Kw</div>
+                    </div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <div class="small text-white stretched-link">Electricity Generated</div>
+                        <div class="small text-white"><i class="fas fa-angle-right"></i> 99 Kw</div>
+                    </div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <div class="small text-white stretched-link">Heat Generated</div>
+                        <div class="small text-white"><i class="fas fa-angle-right"></i> 99 Kw</div>
+                    </div>
+                </div>                
+            </div> 
         </div>
     </div>
     <div class="row">

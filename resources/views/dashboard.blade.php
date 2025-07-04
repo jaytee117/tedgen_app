166cc36196b16f0d9d5bebc3073c5a59c2c2e39a
jaytee117 <jaytee117@gmail.com>
2025-07-04 12:54:43 +0100

    dashboard edit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,49 @@
     <h3 class="mt-4">Dashboard</h3>
     <div class="row">
         <div class="col-xl-3 col-md-6">
-            <div class="card bg-primary text-white mb-4">
+             <div class="card card-price cardblue">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="" style="width: 100px;margin: 0 auto;">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="h2" style="font-size:24px;font-weight:100;margin-top:12px">
+                                Today
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>Gas Consumed</p>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="h99 pull-right"> kW</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>Electricity Generated</p>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="h99 pull-right">kW</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>Heat Generated</p>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="h99 pull-right"> kWth</div>
+                        </div>
+                    </div>
+                </div>
+            <!--<div class="card bg-primary text-white mb-4">
                 <div class="card-body">Primary Card</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
                     <a class="small text-white stretched-link" href="#">View Details</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
-            </div>
+            </div>-->
         </div>
         <div class="col-xl-3 col-md-6">
             <div class="card bg-warning text-white mb-4">

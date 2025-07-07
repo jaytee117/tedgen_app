7cdd37faa280a0ef959daefc5c04cc4bd78af913
JayTee <jaytee117@gmail.com>
2025-07-07 15:15:07 +0100

    edit layout

--- resources/views/theme/default.blade.php
+++ resources/views/theme/default.blade.php
@@ -9,18 +9,22 @@
     <meta name="author" content="" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Dashboard - TedGeneration</title>
-     <script src="https://code.highcharts.com/highcharts.src.js"></script>
+    <script src="https://code.highcharts.com/highcharts.src.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="{{ asset('theme/css/styles.css') }}" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <!--Datatables Scripts/styles-->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.min.css" />
-        <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-       
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 </head>
+
 <body class="sb-nav-fixed">
     @include('theme.header')
     <div id="layoutSidenav">

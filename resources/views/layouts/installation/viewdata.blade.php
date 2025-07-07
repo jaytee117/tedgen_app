d305f232ecee7f5f04e3e444ffee59605607b025
JayTee <jaytee117@gmail.com>
2025-07-07 15:32:57 +0100

    test commit- modifying graphing

--- resources/views/layouts/installation/viewdata.blade.php
+++ resources/views/layouts/installation/viewdata.blade.php
@@ -84,11 +84,11 @@
                         }
                     },
                     series: [{
-                        name: 'Active',
+                        name: 'Heat Generated',
                         data: heat
                     }, {
-                        name: 'Trial',
-                        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+                        name: 'Electricity Generated',
+                        data: elec
                     }]
                 });
             },

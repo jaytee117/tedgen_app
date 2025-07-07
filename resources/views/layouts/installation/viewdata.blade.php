7167bee8ed2608ddf24773e2c456316d705aa008
JayTee <jaytee117@gmail.com>
2025-07-07 15:21:45 +0100

    test

--- resources/views/layouts/installation/viewdata.blade.php
+++ resources/views/layouts/installation/viewdata.blade.php
@@ -57,50 +57,50 @@
             },
             drawBarChart: function(xaxis, elec, gas, heat, elecinput, gasinput) {
                 console.log(xaxis);
-                const chart = Highcharts.chart('container', {
-            chart: {
-                type: 'line'
-            },
-            title: {
-                text: 'Active and Trail users'
-            },
-            subtitle: {
-                text: ''
-            },
-            xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
-                    'Oct', 'Nov', 'Dec'
-                ]
-            },
-            yAxis: {
-                title: {
-                    text: 'Users'
-                }
-            },
-            plotOptions: {
-                line: {
-                    dataLabels: {
-                        enabled: true
+                const chart = Highcharts.chart('barchart-chp', {
+                    chart: {
+                        type: 'line'
                     },
-                    enableMouseTracking: false
-                }
-            },
-            series: [{
-                name: 'Active',
-                data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9,
-                    9.6
-                ]
-            }, {
-                name: 'Trial',
-                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-            }]
-        });
+                    title: {
+                        text: 'Active and Trail users'
+                    },
+                    subtitle: {
+                        text: ''
+                    },
+                    xAxis: {
+                        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
+                            'Oct', 'Nov', 'Dec'
+                        ]
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Users'
+                        }
+                    },
+                    plotOptions: {
+                        line: {
+                            dataLabels: {
+                                enabled: true
+                            },
+                            enableMouseTracking: false
+                        }
+                    },
+                    series: [{
+                        name: 'Active',
+                        data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9,
+                            9.6
+                        ]
+                    }, {
+                        name: 'Trial',
+                        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+                    }]
+                });
             },
         }
     </script>
     <script>
-    document.addEventListener('DOMContentLoaded', function () {
-        ChpDash.initVue();
-    });
-</script>
+        document.addEventListener('DOMContentLoaded', function() {
+            ChpDash.initVue();
+        });
+    </script>
 @endsection

51ac8bc2dd4b65745fc1b84928db8ba975efb203
JayTee <jaytee117@gmail.com>
2025-07-07 15:02:45 +0100

    moved highhcharts

--- resources/views/theme/default.blade.php
+++ resources/views/theme/default.blade.php
@@ -9,6 +9,7 @@
     <meta name="author" content="" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Dashboard - TedGeneration</title>
+     <script src="https://code.highcharts.com/highcharts.src.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="{{ asset('theme/css/styles.css') }}" rel="stylesheet" />
@@ -16,7 +17,7 @@
     <!--Datatables Scripts/styles-->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.min.css" />
         <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-        <script src="https://code.highcharts.com/highcharts.src.js"></script>
+       
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 </head>
